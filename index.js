d6b60494ba0281fdf40b7da337c73c9b2a5e567f
sunitee <suniteebajpai2@gmail.com>
2021-04-02 20:33:13 -0400

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+// function divide( a, b) {
+//  return a / b
+// }
+// let a = 2000
+// let b = 100
+
+// divide(a/b)
+
+
+// let divide = (a, b) =>  {return a / b}
+
+// divide(a,b)
+
+let divide = () => 2000/100
+
+let square = a => a * a
+
+let add = (a , b) => a + b
+
